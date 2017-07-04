af89a7ec5da3c175c66e52af544dd285ab438c0c
y5RjpEqY <gfxv5e@gmail.com>
2017-07-05 00:53:52 +0900

    カウンター作ってみた

--- app/src/main/java/com/example/a/kotlinlin/MainActivity.kt
+++ app/src/main/java/com/example/a/kotlinlin/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.a.kotlinlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var sum = 0
+        var button = findViewById(R.id.button) as Button
+        var textView = findViewById(R.id.tv1) as TextView
+        textView.text = sum.toString()
+
+        button.setOnClickListener { view ->
+            sum ++
+            textView.setText(sum.toString())
+            Log.d("Test",sum.toString())
+        }
     }
 }
