d22e8ecc65bc01d7a3a56aa1584e5d9e377e28f6
y5RjpEqY <gfxv5e@gmail.com>
2017-07-12 00:06:53 +0900

    途中 getSupportFragmentManagerが呼び出せない キレそう

--- app/src/main/java/com/example/a/kotlinlin/MainActivity.kt
+++ app/src/main/java/com/example/a/kotlinlin/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.a.kotlinlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewPager = findViewById(R.id.viewPager) as ViewPager
+        val tabLayout = findViewById(R.id.tabLayout) as TabLayout
+
+        tabLayout.setupWithViewPager(viewPager)
+
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(state: Int) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                Log.v("test","test")
+            }
+
+            override fun onPageSelected(position: Int) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+
+        manager = getSupportFragmentManager()
+
+
+
     }
+
 }
